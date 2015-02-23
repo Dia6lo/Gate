6664e248b1ffa816be4add279d5d494d8914fed0
Dia6lo <Dia6lo@mail.ru>
2015-02-24 09:05:03 +1100

    Half done surroundings

--- sharpServer/Engine/Services/WorldService.cs
+++ sharpServer/Engine/Services/WorldService.cs
@@ -5,6 +5,74 @@ namespace SharpServer.Engine.Services
 {
     internal static class WorldService
     {
+        private static class Surroundings
+        {
+            public struct Entity
+            {
+                public int entity;
+                public string type;
+
+                public Entity(int entity, string type)
+                {
+                    this.entity = entity;
+                    this.type = type;
+                }
+            }
+
+            public struct Cell
+            {
+                public Entity[] entities;
+                public string floorType;
+                public Cell(string floorType, Entity[] entities)
+                {
+                    this.floorType = floorType;
+                    this.entities = entities;
+                }
+            }
+
+            public static Cell[,] GetSurroundings(int entity, int radius)
+            {
+                var size = radius * 2 + 1;
+                var result = new Cell[size, size];
+                for (int i = 0; i < size; i++)
+                    for (int j = 0; j < size; j++)
+                        result[i, j] = new Cell("Void", null);
+                var center = EntityManager.GetComponent<Transform>(entity).Position;
+                result[radius + 1, radius + 1] = MakeCell(center);
+                int l;
+                for (l = 0; (center.X - l > 0) && (l < radius); l ++)
+                {
+                    result[radius - l, radius + 1] = MakeCell(new Vector2(center.X - l, radius + 1));
+                }
+                int r;
+                for (r = 0; (center.X + r < size) && (r < radius); r++)
+                {
+                    result[radius + 2 + r, radius + 1] = MakeCell(new Vector2(center.X + r + 1, radius + 1));
+                }
+                // Half done
+                var tiles = new Cell[TilesX, TilesY];
+                for (var x = 0; x < TilesX; x++)
+                {
+                    for (var y = 0; y < TilesY; y++)
+                    {
+                        var tile = EntityManager.GetComponent<Tile>(Tiles[x, y]);
+                        var entities = tile.Entities
+                            .ConvertAll<Entity>(id => new Entity(id, EntityManager.GetComponent<Render>(id).Type))
+                            .ToArray();
+                        tiles[x, y] = new Cell(tile.FloorType, entities);
+                    }
+                }
+                return tiles;
+            }
+            private static Cell MakeCell (Vector2 position)
+            {
+                var tile = EntityManager.GetComponent<Tile>(WorldService.Tiles[position.X, position.Y]);
+                var entities = tile.Entities
+                            .ConvertAll<Entity>(id => new Entity(id, EntityManager.GetComponent<Render>(id).Type))
+                            .ToArray();
+                return new Cell(tile.FloorType, entities);
+            }
+        }
         static public int TilesX = 20;
         static public int TilesY = 10;
         static public int[,] Tiles = new int[TilesX, TilesY];
@@ -50,46 +118,6 @@ namespace SharpServer.Engine.Services
         {
             RemoveEntity(entity);
             AddEntity(position, entity);
-        }
-
-         public struct Entity
-        {
-            public int entity;
-            public string type;
-
-            public Entity(int entity, string type)
-            {
-                this.entity = entity;
-                this.type = type;
-            }
-        }
-
-        public struct Cell
-        {
-            public Entity[] entities;
-            public string floorType;
-            public Cell(string floorType, Entity[] entities)
-            {
-                this.floorType = floorType;
-                this.entities = entities;
-            }
-        }
-
-        public static Cell[,] GetSurroundings(int entity, int radius)
-        {
-            var tiles = new Cell[TilesX, TilesY];
-            for (var x = 0; x < TilesX; x++)
-            {
-                for (var y = 0; y < TilesY; y++)
-                {
-                    var tile = EntityManager.GetComponent<Tile>(Tiles[x, y]);
-                    var entities = tile.Entities
-                        .ConvertAll<Entity>(id => new Entity(id, EntityManager.GetComponent<Render>(id).Type))
-                        .ToArray();
-                    tiles[x, y] = new Cell(tile.FloorType, entities);
-                }
-            }
-            return tiles;
         }
 
         static public void RemoveEntity(int entity)
